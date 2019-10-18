11b6e1a2068cf5dee1039a7a2b2e478fd9005b46
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-10-18 13:28:29 -0600

    save

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -5,6 +5,14 @@ module.exports = {
     test: (req, res, next) => {
         res.status(200).send('you got me')
     },
-    
+    createMessage : (req, res, next) => {
+        const {text, time} = req.body
+        messages.push({
+            text: text,
+            time: time
+        })
+        id++
+        res.status(200).send(messages)
+    }
 
 }
